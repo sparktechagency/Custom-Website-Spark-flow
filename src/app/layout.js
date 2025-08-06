1ec9ab6378cbd436942047d388790f5295bd888c
nimurr <nimurnerob404@gmail.com>
2025-08-06 10:58:01 +0600

    add fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head >
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet" />
+
+      </ head >
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -34,6 +41,6 @@ export default function RootLayout({ children }) {
         />
         {children}
       </body>
-    </html>
+    </html >
   );
 }

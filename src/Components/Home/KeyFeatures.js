432fd84a0986258e7ad687e22121acf3376b1c2b
nimurr <nimurnerob404@gmail.com>
2025-08-07 13:13:20 +0600

    add infor of key features page

--- src/Components/Home/KeyFeatures.js
+++ src/Components/Home/KeyFeatures.js
@@ -8,10 +8,14 @@ const KeyFeatures = () => {
                 <p>Explore the powerful tools and features that help contractors find skilled subcontractors, manage projects, and get work done efficiently. Whether you're posting a job or searching for your next opportunity, we make it easy to connect, collaborate, and succeed.</p>
             </div>
             <div className='grid xl:grid-cols-2 gap-5 mt-10'>
-                <div>
-                    <h2 className='text-2xl font-bold text-gray-700 text-center'>Contractor Features</h2>
-                    <div>
-                        
+                <div className='relative'>
+                    <img className='relative' src="/Images/Home/keyfeaturesBG.png" alt="" />
+                    <div className='absolute top-0 w-full left-0 p-10 '>
+                        <h2 className='text-2xl font-bold text-gray-700 text-center mb-5'>Contractor Features</h2>
+                        <div className='bg-[url("/Images/Home/features-tems-bg.png")] bg-no-repeat object-fill bg-contain  !min-h-48 py-3 px-5'>
+                            <h3 className='text-xl font-bold text-gray-700 mb-1'>1. Post Jobs</h3>
+                            <p className='text-gray-700 text-sm'>Easily post job opportunities by detailing project scope, required skills, and deadlines. Get your project up and running in no time.</p>
+                        </div>
                     </div>
                 </div>
                 <div>

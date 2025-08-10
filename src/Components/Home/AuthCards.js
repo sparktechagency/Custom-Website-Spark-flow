6b6db4744b360aa50089be235451b752d14091c4
nimurr <nimurnerob404@gmail.com>
2025-08-10 10:30:12 +0600

    add auth card

---
+++ src/Components/Home/AuthCards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AuthCards = () => {
+    return (
+        <section>
+            <div>
+                 
+            </div>
+        </section>
+    );
+}
+
+export default AuthCards;

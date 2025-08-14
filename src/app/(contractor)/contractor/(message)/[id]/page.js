15b6d03366015190e49c1c68938b001cc9929d22
nimurr <nimurnerob404@gmail.com>
2025-08-14 10:40:08 +0600

    modify messasge box field and image upload field

--- src/app/(contractor)/contractor/(message)/[id]/page.js
+++ src/app/(contractor)/contractor/(message)/[id]/page.js
@@ -1,4 +1,5 @@
 'use client';
+import icons from '@/icons/icon';
 import { useParams } from 'next/navigation';
 import React, { useState } from 'react';
 import { FaPaperclip } from 'react-icons/fa'; // For the attachment icon
@@ -40,7 +41,7 @@ const MessageInput = () => {
                 <div className='flex items-end justify-end space-x-2'>
                     <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                    </p> 
+                    </p>
                 </div>
                 <div className='flex items-start space-x-2'>
                     <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
@@ -53,8 +54,8 @@ const MessageInput = () => {
                 </div>
                 <div className='flex items-end justify-end space-x-2'>
                     <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
-                       I want to work with you
-                    </p> 
+                        I want to work with you
+                    </p>
                 </div>
                 <div className='flex items-start space-x-2'>
                     <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
@@ -65,32 +66,7 @@ const MessageInput = () => {
                 <div className='flex items-end justify-end space-x-2'>
                     <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                    </p> 
-                </div>
-                <div className='flex items-start space-x-2'>
-                    <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
-                    <p className='text-sm w-2/3 max-w-[600px] bg-blue-100 rounded-lg p-2'>
-                        Demo ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                        demo message 100 word need to write
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
                     </p>
-                </div>
-                <div className='flex items-end justify-end space-x-2'>
-                    <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
-                       I want to work with you
-                    </p> 
-                </div>
-                <div className='flex items-start space-x-2'>
-                    <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
-                    <p className='text-sm w-2/3 max-w-[600px] bg-blue-100 rounded-lg p-2'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                    </p>
-                </div>
-                <div className='flex items-end justify-end space-x-2'>
-                    <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
-                    </p> 
                 </div>
                 <div className='flex items-start space-x-2'>
                     <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
@@ -103,32 +79,63 @@ const MessageInput = () => {
                 </div>
                 <div className='flex items-end justify-end space-x-2'>
                     <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
-                       I want to work with you
-                    </p> 
+                        I want to work with you
+                    </p>
+                </div>
+                <div className='flex items-start space-x-2'>
+                    <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
+                    <p className='text-sm w-2/3 max-w-[600px] bg-blue-100 rounded-lg p-2'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
+                    </p>
+                </div>
+                <div className='flex items-end justify-end space-x-2'>
+                    <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
+                    </p>
+                </div>
+                <div className='flex items-start space-x-2'>
+                    <img className='w-10 h-10 rounded-lg' src="https://randomuser.me/api/portraits/men/41.jpg" alt="" />
+                    <p className='text-sm w-2/3 max-w-[600px] bg-blue-100 rounded-lg p-2'>
+                        Demo ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
+                        demo message 100 word need to write
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur eaque blanditiis veniam, cum modi?
+                    </p>
+                </div>
+                <div className='flex items-end justify-end space-x-2'>
+                    <p className='text-sm w-2/3 max-w-[600px] bg-green-100 rounded-lg p-2'>
+                        I want to work with you
+                    </p>
                 </div>
 
             </div>
 
             {/* Message Input Box */}
             <div>
-                {image && (
-                    <div className="mt-4">
-                        <img src={image} alt="Uploaded Preview" className="w-20 h-20 object-cover rounded-md" />
-                    </div>
-                )}
+
                 <div className="flex items-center border-2 border-gray-300 rounded-lg py-1 pl-3 pr-2 mt-4">
                     {/* Attachment Icon */}
-                    <label htmlFor="file-upload" className="text-gray-500 cursor-pointer">
-                        <FaPaperclip />
-                        {/* Invisible file input */}
-                        <input
-                            id="file-upload"
-                            type="file"
-                            accept="image/*"
-                            onChange={handleImageChange}
-                            className="hidden"
-                        />
-                    </label>
+
+
+                    {image ? (
+                        <div className="relative">
+                            <img src={image} alt="Uploaded Preview" className="w-12 border border-gray-300 object-cover rounded-md" />
+                            <span onClick={() => setImage(null)} className='absolute -top-1 -right-2 cursor-pointer rounded-lg flex items-center justify-center text-sm bg-red-200 w-6 h-6'>{icons.cross}</span>
+                        </div>
+                    )
+                        :
+                        <label htmlFor="file-upload" className="text-gray-500 cursor-pointer">
+                            <FaPaperclip />
+                            {/* Invisible file input */}
+                            <input
+                                id="file-upload"
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                                className="hidden"
+                            />
+                        </label>
+                    }
 
                     {/* Text Input */}
                     <input

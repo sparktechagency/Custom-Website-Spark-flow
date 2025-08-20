c5701ece5d7cda53ba93400909e2beeab8b04abb
nimurr <nimurnerob404@gmail.com>
2025-08-20 17:24:37 +0600

    everything is up to date

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  output: 'standalone',
-};
+const nextConfig = {};
 
 export default nextConfig;
